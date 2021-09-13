c6ef4c6ab95f1ff5a75c3cf0210e3441f13a6ec7
AYAKO <ainnaquartz@gmail.com>
2021-09-13 19:08:26 +0900

    20210913-19:08

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>COACHTECH</title>
-  <link href="{{asset('/resources/css/reset.css')}}" rel="stylesheet">
-  <link  href="{{asset('/resources/css/style.css')}}" rel="stylesheet">
+  <link href="css/reset.css" rel="stylesheet">
+  <link  href="css/style.css" rel="stylesheet">
 </head>
 
 <body>
